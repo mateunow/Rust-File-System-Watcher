c54979b7f7a0a099d7ed719885d371135abd27f8
mateunow <mateuszjestemja90@gmail.com>
2025-06-03 23:29:18 +0200

    Added extended information about changed files, names of the files and distinction between files and folders.

--- rust_project/src/main.rs
+++ rust_project/src/main.rs
@@ -1,53 +1,107 @@
 use std::env;
 use std::path::Path;
-use notify::{event::{AccessKind, RenameMode}, RecursiveMode, Result, Watcher};
+use notify::{event::{AccessKind, RemoveKind}, RecursiveMode, Result, Watcher};
+use notify::event::AccessMode;
 
-// fn main() 
+
 fn main() -> Result<()>
 {
-    // let path = Path::new("./foo/bar/bar.txt");
-
-    // let parent = path.parent();
-    // print!("{:?}", parent);
-    // assert_eq!(parent, Some(Path::new("./bar")));
     let args: Vec<String> = env::args().collect();
     let path = Path::new(&args[1]);
-    println!("Start: {:?}", path);
+    println!("Starting watcher on path: {:?}", path);
     start_watching(path)?;
     Ok(())
 }
 
 fn start_watching(path: &Path) -> Result<()>
 {
-    let mut watcher = notify::recommended_watcher(handle_event)?;
+    let base_path = path.to_path_buf();
+    let mut watcher = notify::recommended_watcher(move |event| handle_event(event, &base_path))?;
     watcher.watch(path, RecursiveMode::Recursive)?;
     println!("Observing path: {:?}", path);
     loop {
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
-fn handle_event(result: notify::Result<notify::Event>)
+fn handle_event(result: notify::Result<notify::Event>, base_path: &Path)
 {
     //Result<T, E> - 'hej mam coś, ale może to być ok(), albo err()
-    //println!("Got event: {:?}", result);
     match result {
         Ok(event) => {
-            println!("Got event: {:?}", event);
-            // let parent = event.paths.to;
-            // print!("Parent path: {:?}", parent);
+            if let Some(path) = event.paths.last() {
+                let rel_path = path.strip_prefix(base_path).unwrap_or(path);
+                if let Some(name) = path.file_name() {
+                    for path in &event.paths {
+                                if path.is_file() {
+                                    println!("File: {:?}", name);
+                                } else if path.is_dir() {
+                                    println!("Folder: {:?}", name);
+                                } else {
+                                    println!("Unknown: {:?}", name);
+                                }
+                            }
+                } else {
+                    println!("Path has no file name: {:?}", rel_path);
+                }
+            } else {
+                println!("No paths in event");
+            }
             match event.kind {
-                notify::EventKind::Access(AccessKind::Close(_)) => {
-                    
+                notify::EventKind::Access(AccessKind::Close(access)) => {
+                    match access {
+
+                        AccessMode::Read => {
+                            if let Some(path) = event.paths.last() {
+                                println!("File read closed in path: {:?}", path);
+                            }
+                        }
+                        AccessMode::Write => {
+                            if let Some(path) = event.paths.last() {
+                                println!("File write closed in path: {:?}", path);
+                            }
+                        }
+                        AccessMode::Execute =>
+                         if let Some(path) = event.paths.last() {
+                                println!("File execute closed in path: {:?}", path);
+                            }
+                        _ => {}
+                    }
                     println!("File closed in path: {:?}", event.paths);
                 }
-                notify::EventKind::Create(_) => {
-                    println!("File created in path: {:?}", event.paths);
+                notify::EventKind::Create(kind) => {
+                    match kind {
+                        notify::event::CreateKind::File => {
+                            println!("File created in path: {:?}", event.paths);
+                        }
+                        notify::event::CreateKind::Folder => {
+                            println!("Folder created in path: {:?}", event.paths);
+                        }
+                        notify::event::CreateKind::Any => {
+                            for path in &event.paths {
+                                if path.is_file() {
+                                    println!("File created in path: {:?}", path);
+                                } else if path.is_dir() {
+                                    println!("Folder created in path: {:?}", path);
+                                } else {
+                                    println!("Unknown created in path: {:?}", path);
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
                 }
+                
                 notify::EventKind::Modify(_) => {
-                    println!("File modified in path: {:?}", event.paths);
+                    println!("File modified in path: {:?}", event.paths[0]);
                 }
-                notify::EventKind::Remove(_) => {
+                notify::EventKind::Remove(RemoveKind::File) => {
                     println!("File removed in path: {:?}", event.paths);
+                }
+                notify::EventKind::Remove(RemoveKind::Folder) => {
+                    println!("Folder removed in path: {:?}", event.paths);
+                }
+                notify::EventKind::Remove(RemoveKind::Any) => {
+                    println!("File or Folder removed in path: {:?}", event.paths);
                 }
                 _ => {}
             }
